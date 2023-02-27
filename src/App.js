b9c6e3723822771e5955850dd55d43af74f0c9f5
duonghuynguyen <89695971+duonghuynguyen@users.noreply.github.com>
2023-02-27 08:32:17 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import logo from "./logo.svg";
+// import { useEffect } from "react";
 import "./App.css";
-import GoogleLogin from "react-google-login";
-
+// import GoogleLogin from "react-google-login";
+import { GoogleLogin } from "@react-oauth/google";
 function App() {
-  const handleLogin = (data) => {
-    console.log("data", data);
-  };
-  const handleFailure = (response) => {
-    console.log("response", response);
-  };
+  // const handleLogin = (data) => {
+  //   console.log("data", data);
+  // };
+  // const handleFailure = (response) => {
+  //   console.log("response", response);
+  // };
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Google Login App</h1>
         <div>
           <GoogleLogin
-            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-            buttonText="Log in with Google"
-            onSuccess={handleLogin}
-            onFailure={handleFailure}
-            cookiePolicy={"single_host_origin"}
-          ></GoogleLogin>
+            onSuccess={(credentialResponse) => {
+              console.log(credentialResponse);
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+          />
         </div>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="525959507300-1r5q645rhn0rkikrisbt2hus5ispug9c.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
