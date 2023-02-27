7b45cc1f5367099882ed2fe7f4fee54fd95218bd
duonghuyom <huy.nguyen20@onemount.com>
2023-02-27 08:18:41 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import GoogleLogin from "react-google-login";
 
 function App() {
+  const handleLogin = (data) => {
+    console.log("data", data);
+  };
+  const handleFailure = (response) => {
+    console.log("response", response);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Google Login App</h1>
+        <div>
+          <GoogleLogin
+            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+            buttonText="Log in with Google"
+            onSuccess={handleLogin}
+            onFailure={handleFailure}
+            cookiePolicy={"single_host_origin"}
+          ></GoogleLogin>
+        </div>
       </header>
     </div>
   );
